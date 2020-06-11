da41576d2efcafdc7a73ccf5f3985f560eae5738
Muhammad Minhal Javed <minhal47@gmail.com>
2020-06-11 15:50:01 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <br/>
-          Updated by MInhal Javed.
+          Updated by MInhal Javed
 
         </p>
         <a
