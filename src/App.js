60f7c6441a184eca0aa13194cfc8975b1c81b4f9
Muhammad Minhal Javed <minhal47@gmail.com>
2020-06-11 15:49:35 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br/>
+          Updated by MInhal Javed.
+
         </p>
         <a
           className="App-link"
